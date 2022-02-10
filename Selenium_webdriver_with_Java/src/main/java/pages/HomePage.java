6759b00161769b12e51a4ec58e2cc5d3a0e8f507
emis <emisnps@gmail.com>
2022-02-10 10:39:51 +0100

    feat: chapter 9 is done

--- Selenium_webdriver_with_Java/src/main/java/pages/HomePage.java
+++ Selenium_webdriver_with_Java/src/main/java/pages/HomePage.java
@@ -56,6 +56,11 @@ public class HomePage {
         return new WysiwygEditorPage(driver);
     }
 
+    public DynamicLoadingPage clickDynamicLoading(){
+        clickLink("Dynamic Loading");
+        return new DynamicLoadingPage(driver);
+    }
+
     private void clickLink(String linkText){
         driver.findElement(By.linkText(linkText)).click();
     }
