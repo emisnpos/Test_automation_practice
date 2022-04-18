9261c2a513200a8bb468b47b8383639f1f5c97a0
emis <emisnps@gmail.com>
2022-04-18 23:46:52 +0200

    Feature - practice with assertTrue, assertThrows, assertArrayEquals, assertEquals and assertAll

---
+++ jUnit_5_practice/src/test/java/AssertionsTest.java
@@ -0,0 +1,55 @@
+import listeners.Listener;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(Listener.class)
+public class AssertionsTest {
+
+    @Test
+    void assertEqualsTest() {
+        assertEquals("firstString", "secondString", "The String " +
+                "values were not equal");
+    }
+
+    @Test
+    void assertEqualsListTest() {
+        List<String> expectedValues = Arrays.asList("firstString",
+                "secondString", "thirdString");
+        List<String> actualValues = Arrays.asList("firstString",
+                "secondString");
+        assertEquals(expectedValues, actualValues);
+    }
+
+    @Test
+    void assertArraysEqualsTest() {
+        int[] expectedValues = {1, 5, 3};
+        int[] actualValues = {1, 2, 3};
+        assertArrayEquals(expectedValues, actualValues);
+    }
+
+    @Test
+    void assertTrueTest() {
+        assertFalse(false);
+        assertTrue(false, "This boolean condition did not evaluate to true");
+    }
+
+    @Test
+    void assertThrowsTest() {
+        assertThrows(NullPointerException.class, null);
+    }
+
+    @Test
+    void assertAllTest() {
+        assertAll(
+                () -> assertEquals("firstString", "secondString", "The String " +
+                        "values were not equal"),
+                () -> assertThrows(NullPointerException.class, null),
+                () -> assertTrue(false, "This boolean condition did not evaluate to true"));
+    }
+
+}
