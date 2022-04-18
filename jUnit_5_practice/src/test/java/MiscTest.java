a8a22968e1dea609ffa629372b7f8b67dc4bf4a5
emis <emisnps@gmail.com>
2022-04-18 23:54:58 +0200

    Feature - practice with @Nested tests, MyAnnotation test, @Timeout()

---
+++ jUnit_5_practice/src/test/java/MiscTest.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.*;
+
+
+public class MiscTest {
+
+    @Test
+    @Timeout(5) // index = seconds
+    void timeout() throws InterruptedException {
+        System.out.println("This is the test with the timeout");
+        Thread.sleep(4000);
+        //Thread.sleep(6000);
+    }
+
+    @Test
+    @Timeout(90)
+    @DisplayName("This is the nice method")
+    @Tag("theTag")
+    void annotatedMethod1() {
+        System.out.println("This is the annotated method");
+    }
+
+    @MyAnnotation
+    void annotatedMethod2() throws InterruptedException {
+        System.out.println("This is the custom annotated method");
+        Thread.sleep(3000);
+    }
+
+
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    @Nested
+    class NestedTest {
+
+        @BeforeAll
+        void beforeAll() {
+            System.out.println("Before All in nested test");
+        }
+
+        @Test
+        void nestedTestMethod() {
+            System.out.println("Nested test method");
+        }
+    }
+}
